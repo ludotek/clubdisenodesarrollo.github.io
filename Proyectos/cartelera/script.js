0b97b360bf2b98ae1ac6a0f57aa63618b5e8ea24
clubdedesarrollo <clubdedesarrollo1@gmail.com>
2024-06-06 02:37:59 -0500

    commit

--- Proyectos/cartelera/script.js
+++ Proyectos/cartelera/script.js
@@ -5,8 +5,8 @@ window.onscroll = function(){
     var elemento1 = document.getElementById("icon_pop");
     var elemento2 = document.getElementById("icon_locura");
 
-    elemento1.style.bottom = posicion * 0.1 + "px";
-    elemento2.style.top = posicion * 0.02 + "px";
+    elemento1.style.bottom = posicion * 0.2 + "px";
+    elemento2.style.top = posicion * 0.05 + "px";
 
 }
 
