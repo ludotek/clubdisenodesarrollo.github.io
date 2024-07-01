5c9116efbdb3e5fd85ee79170c03aab02e2df6f4
clubdedesarrollo <clubdedesarrollo1@gmail.com>
2024-06-30 22:05:41 -0500

    commit

--- Proyectos/seBuscan/script.js
+++ Proyectos/seBuscan/script.js
@@ -1,51 +1,3 @@
-const openModal = document.querySelector('.hero__cta');
-const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.modal__close');
-
-openModal.addEventListener('click', (e) => {
-    e.preventDefault();
-    modal.classList.add('modal--show');
-    mensaje.style.display = none;
-});
-
-closeModal.addEventListener('click', (e) => {
-    e.preventDefault();
-    modal.classList.remove('modal--show');
-});
-
-
-
-const openModal2 = document.querySelector('.hero__cta2');
-const modal2 = document.querySelector('.modal2');
-const closeModal2 = document.querySelector('.modal__close2');
-
-openModal2.addEventListener('click', (e) => {
-    e.preventDefault();
-    modal2.classList.add('modal--show');
-});
-
-closeModal2.addEventListener('click', (e) => {
-    e.preventDefault();
-    modal2.classList.remove('modal--show');
-});
-
-
-
-
-
-const openModalSD = document.querySelector('.hero__ctaSD');
-const modalSD = document.querySelector('.modalSD');
-const closeModalSD = document.querySelector('.modal__closeSD');
-
-openModalSD.addEventListener('click', (e) => {
-    e.preventDefault();
-    modalSD.classList.add('modal--show');
-});
-
-closeModalSD.addEventListener('click', (e) => {
-    e.preventDefault();
-    modalSD.classList.remove('modal--show');
-});
 
 
 
@@ -53,6 +5,25 @@ closeModalSD.addEventListener('click', (e) => {
 
 
 
+let modalSD = document.getElementById("modalSD");
+let modalAZY = document.getElementById("modalAZY");
 
+function verDetalle(numeroCard) {
+    switch (numeroCard) {
+        case 1:
+            modalSD.setAttribute("style", "display: flex");
+            modalAZY.setAttribute("style", "display: flex");
 
+            break;
+        case 2:
+            modalSD.setAttribute("style", "display: flex");
 
+            modalAZY.setAttribute("style", "display: none");
+            break;
+        case 3:
+            modalAZY.setAttribute("style", "display: flex");
+
+            modalSD.setAttribute("style", "display: none");
+            break;
+    }
+}
