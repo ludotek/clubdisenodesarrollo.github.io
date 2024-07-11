739689ec007e7198fbba1418ff8c25d03b538668
clubdisenodesarrollo <clubdedesarrollo1@gmail.com>
2024-07-11 15:07:51 -0500

    commit

--- Proyectos/ludotek/script.js
+++ Proyectos/ludotek/script.js
@@ -104,7 +104,21 @@ teamMemberPhotos.forEach(photo => {
 
 
 
+const openModal = document.querySelector('.hambur');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__close');
 
+openModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
+
+});
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+
+});
 
 
 
